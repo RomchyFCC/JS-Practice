486fece1b0a79dc5cf9aea3f219889eb50990214
romchy <roman.struna.dev@gmail.com>
2018-02-28 20:04:46 +0100

    adding the server side to exercise08

--- Exercise08/chat/src/index.js
+++ Exercise08/chat/src/index.js
@@ -4,11 +4,27 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import {addUser} from './actions';
+import setupSocket from './sockets';
+import createSagaMiddleware from 'redux-saga';
+import handleNewMessage from './sagas';
+import username from './utils/name'
 
-import chat from './reducers';
+import reducers from './reducers';
 
-const store = createStore(chat);
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+  reducers,
+  applyMiddleware(sagaMiddleware)
+);
+
+const socket = setupSocket(store.dispatch, username);
+
+sagaMiddleware.run(handleNewMessage, {socket, username})
+
+store.dispatch(addUser('Me'))
 
 ReactDOM.render(
   <Provider store={store}>
